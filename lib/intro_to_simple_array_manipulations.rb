1a1437a47971ff6151bc0a17c0b2ac3bf8bb3e2f
assane savadogo <assane.savadogo81@gmail.com>
2019-06-01 04:19:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,6 +26,6 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, element)
-  new_array = new
-  array.insert(3, element)
+  new_array = "Malbolge"
+  array.insert(3, "Malbolge")
 end
